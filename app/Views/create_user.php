8bdbb14c5c94074483bb08779342a6e0efe1cd44
unknown <dwikapangestuti999@gmail.com>
2023-10-10 07:53:38 +0700

    upload

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -1,22 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="<?=base_url("assets/css/style.css")?>">
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet'>
-</head>
-<body>
-    <div class="container"> 
-        <form action="<?= base_url('/user/store')?>" method="post">
+<?= $this->extend('layout/app') ?>
+
+<?= $this->section('content') ?>
+   <div class="container"> 
+        <form action="<?= base_url('/user/store')?>" method="post" enctype="multipart/form-data">
             <h3>Form Biodata</h3>
             <form action="<?= base_url('/user/store') ?>" method="POST">
                 <input type="text" name="nama" placeholder="Nama" class="form-control <?= ($validation->hasError('nama')) ?
                 'is-invalid' : ''; ?>" id="nama" autofocus>
                 <div class="invalid-feedback">
                     <?= $validation->getError('nama') ?>
-                </div>
+                </div>
             <fieldset>
                 <select placeholder="Kelas" type="text" name="kelas" tabindex="2" required>
                     <?php 
@@ -35,13 +28,15 @@
                     'is-invalid' : ''; ?>" id="npm" autofocus>
                     <div class="invalid-feedback">
                         <?= $validation->getError('npm') ?>
-                  </div>  
+                    </div>  
                 <!-- <input placeholder="NPM" type="text" name="npm" tabindex="3" required> -->
             </fieldset>
+            <!-- <label class="input-group-text"  for="foto">Upload</label> -->
+                                <input type="file" class="form-control" name='foto'id="foto">
             <fieldset>
                 <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
             </fieldset>
         </form>
     </div>
-</body>
-</html>
+    
+<?= $this->endsection() ?>
